05b1df2e581b4dfb5b1c2809d1859e4d2f670540
novatransformation <kim.heiermeier@gmail.com>
2025-11-15 20:39:59 +0100

    Final Architecture Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  root: 'client',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'client/src'),
     },
   },
+  build: {
+    outDir: '../dist'
+  }
 })
